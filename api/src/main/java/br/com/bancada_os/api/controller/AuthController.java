4bdee9ca288f37d3fce3332414f13b3dee9633bd
João Francisco <joaofrancisco.amello@gmail.com>
2026-02-16 14:46:03 -0300

    feat: nova configuração de rotas auth e geração de tokens

--- api/src/main/java/br/com/bancada_os/api/controller/AuthController.java
+++ api/src/main/java/br/com/bancada_os/api/controller/AuthController.java
@@ -3,30 +3,39 @@ package br.com.bancada_os.api.controller;
 import br.com.bancada_os.api.dto.UsuarioRequestDTO;
 import br.com.bancada_os.api.dto.UsuarioResponseDTO;
 import br.com.bancada_os.api.model.Usuario;
+import br.com.bancada_os.api.service.TokenService;
 import br.com.bancada_os.api.service.UsuarioService;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/usuarios")
-public class UsuarioController {
+@RequestMapping("/auth")
+public class AuthController {
 
     private final UsuarioService service;
 
-    public UsuarioController(UsuarioService service) {
+    public AuthController(UsuarioService service) {
         this.service = service;
     }
 
-    @PostMapping
+    @PostMapping("/cadastro")
     public ResponseEntity<UsuarioResponseDTO> cadastrar(@RequestBody @Valid UsuarioRequestDTO dados, UriComponentsBuilder uriBuilder) {
         var dto = service.cadastrar(dados);
         var uri = uriBuilder.path("/usuarios/{id}").buildAndExpand(dto.idUsuario()).toUri();
         return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody @Valid UsuarioRequestDTO dados) {
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
+        var token = TokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJWT(token));
     }
 
     @GetMapping
---
+++ api/src/main/java/br/com/bancada_os/api/service/TokenService.java
@@ -0,0 +1,35 @@
+package br.com.bancada_os.api.service;
+
+import br.com.bancada_os.api.model.Usuario;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${API.SECURITY.TOKEN.SECRET}")
+    private String secret;
+
+    public static String gerarToken(Usuario usuario){
+        try {
+            var algoritmo = Algorithm.HMAC256(secret);
+            return com.auth0.jwt.JWT.create()
+                    .withIssuer("BancadaOS-API")
+                    .withSubject(usuario.getEmail())
+                    .withClaim("id", usuario.getIdUsuario())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algoritmo);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("Erro ao gerar token JWT", exception);
+        }
+    }
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(4).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
